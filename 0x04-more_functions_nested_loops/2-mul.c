797b841f208b9d1c6084b4cfdc8b27c9f3553c15
Abed25 <officialwekesa254@gmail.com>
2023-09-14 03:41:46 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies tqo integers
+ * @a: parameter one
+ * @b: parameter two
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
