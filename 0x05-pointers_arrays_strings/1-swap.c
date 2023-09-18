6c19dc8a5ca19f2966b32cbc3bbddf8cdda10e8c
Abed25 <officialwekesa254@gmail.com>
2023-09-18 06:54:30 -0700

    swapper

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - does swapping
+ * @a: first pointer
+ * @b: second pointer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
